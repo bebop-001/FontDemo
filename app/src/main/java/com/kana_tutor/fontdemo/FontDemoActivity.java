3913be17486e903f0a5389eb2da04aabc9be6145
sjs <sjs@sonic.net>
2018-09-25 09:38:15 -0700

    Added show of bold, normal, italic.

--- app/src/main/java/com/kana_tutor/fontdemo/FontDemoActivity.java
+++ app/src/main/java/com/kana_tutor/fontdemo/FontDemoActivity.java
@@ -31,37 +31,76 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@SuppressWarnings("SameParameterValue,DefaultLocale")
 public class FontDemoActivity extends AppCompatActivity {
     private static final String TAG = FontDemoActivity.class.getSimpleName();
 
-    private SharedPreferences myPrefs;
+    private static class FontInfo {
+        final String name;
+        final int [] fontInfo;
+        private FontInfo(String name, int[] fontInfo) {
+            this.name = name; this.fontInfo = fontInfo;
+        }
+    }
     private EditText fontEditText;
     private Button fontSelectButton;
     // prefs file for keeping the demo string
     private static final String myPrefsFileName = "myPrefs";
+    private SharedPreferences myPrefs;
     // font resources to demo.
-    static final Map<String, Integer> fontMap = new HashMap<>();
+    private static final Map<String, FontInfo> fontMap = new HashMap<>();
     static {
-        fontMap.put("Takao", R.font.takao_gothic_regular);
-        fontMap.put("HanaMinA", R.font.hana_min_a_regular);
-        fontMap.put("DroidSansJapanese", R.font.droid_sans_japanese_regular);
+        fontMap.put("Takao NORMAL", new FontInfo("Takao NORMAL",
+            new int[]{R.font.takao_gothic_regular, Typeface.NORMAL}));
+        fontMap.put("Takao BOLD", new FontInfo("Takao BOLD",
+            new int[]{R.font.takao_gothic_regular, Typeface.BOLD}));
+        fontMap.put("Takao ITALIC", new FontInfo("Takao ITALIC",
+            new int[]{R.font.takao_gothic_regular, Typeface.ITALIC}));
+        fontMap.put("HanaMinA NORMAL", new FontInfo("HanaMinA NORMAL",
+            new int[]{R.font.hana_min_a_regular, Typeface.NORMAL}));
+        fontMap.put("HanaMinA BOLD", new FontInfo("HanaMinA NORMAL",
+            new int[]{R.font.hana_min_a_regular, Typeface.BOLD}));
+        fontMap.put("HanaMinA ITALIC", new FontInfo("HanaMinA ITALIC",
+            new int[]{R.font.hana_min_a_regular, Typeface.ITALIC}));
+        fontMap.put("DroidSansJapanese NORMAL", new FontInfo("DroidSansJapanese NORMAL" ,
+            new int[]{R.font.droid_sans_japanese_regular, Typeface.NORMAL}));
+        fontMap.put("DroidSansJapanese BOLD", new FontInfo("DroidSansJapanese BOLD",
+            new int[]{R.font.droid_sans_japanese_regular, Typeface.BOLD}));
+        fontMap.put("DroidSansJapanese ITALIC", new FontInfo("DroidSansJapanese ITALIC",
+            new int[]{R.font.droid_sans_japanese_regular, Typeface.ITALIC}));
     }
-    static final List<String> fontNames = new ArrayList<>(fontMap.keySet());
+    private static final List<String> fontNames;
+    static {
+        List <String>l = new ArrayList<>(fontMap.keySet());
+        Collections.sort(l);
+        fontNames = l;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.font_demo);
         myPrefs = getSharedPreferences(myPrefsFileName, Context.MODE_PRIVATE);
+        String fontName = myPrefs.getString("currentFontName", fontNames.get(0));
+        FontInfo currentFontInfo = fontMap.get(fontName);
+        if (currentFontInfo == null) {
+            currentFontInfo = fontMap.get(fontNames.get(0));
+            myPrefs.edit().putString("currentFontName", currentFontInfo.name).apply();
+        }
         String fontDemoString = myPrefs.getString("fontDemoString",
                 "今日は皆さん。");
         fontEditText = findViewById(R.id.font_edit_text);
+        fontEditText.setTypeface(
+                ResourcesCompat.getFont(FontDemoActivity.this,
+                    currentFontInfo.fontInfo[0]), currentFontInfo.fontInfo[1]);
         fontEditText.setText(fontDemoString);
         fontSelectButton =  findViewById(R.id.font_select_button);
+        fontSelectButton.setText(currentFontInfo.name);
         fontSelectButton.setOnClickListener(new View.OnClickListener() {
             // toggle through the possible font selections.
             @Override
@@ -78,25 +117,30 @@ public class FontDemoActivity extends AppCompatActivity {
                 }
                 if (next == null)
                     next = fontNames.get(0);
-                int fontResource = fontMap.get(next);
-                Log.d(TAG, String.format("Font resource:0x%08x", fontResource));
+                FontInfo info = fontMap.get(next);
+                myPrefs.edit().putString("currentFontName", next).apply();
+                Log.d(TAG, String.format(
+                    "Font resource:0x%08x:%d", info.fontInfo[0], info.fontInfo[1]));
                 fontEditText.setTypeface(
-                    ResourcesCompat.getFont(FontDemoActivity.this, fontResource)
-                    , Typeface.BOLD);
+                    ResourcesCompat.getFont(FontDemoActivity.this
+                    , info.fontInfo[0]), info.fontInfo[1]);
                 fontSelectButton.setText(next);
             }
         });
         fontEditText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            public void beforeTextChanged(
+                    CharSequence s, int start, int count, int after) {
             }
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            public void onTextChanged(
+                    CharSequence s, int start, int before, int count) {
             }
             // update the demo string as the user changes it.
             @Override
             public void afterTextChanged(Editable s) {
-                myPrefs.edit().putString("fontDemoString", s.toString()).apply();
+                myPrefs.edit()
+                    .putString("fontDemoString", s.toString()).apply();
             }
         });
     }
